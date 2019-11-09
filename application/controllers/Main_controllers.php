a1886a63dc8bf55add4539ef45e39ff3a0a17bfe
ArchiOrange <dinka4.435@gmail.com>
2019-11-09 17:56:33 +0300

    добавлена функция в контроллер которая получает секции и клубы

--- application/controllers/Main_controllers.php
+++ application/controllers/Main_controllers.php
@@ -5,6 +5,19 @@ class Main_controllers extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('main_view');
+
+	}
+	public function club()
+	{
+		$this->load->modal('Club_model');
+		$data['club'] = $this->Club_model->get_club();
+		print_r($data);
+	}
+	public function sections()
+	{
+		$this->load->modal('Section_model');
+		$data['section'] = $this->Club_model->get_club();
+		print_r($data);
 	}
 }
 ?>
