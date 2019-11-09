3311a63ed7034a196249c87610836577049ebd09
IgorSuslov254 <igirsuslov@gmail.com>
2019-11-09 23:04:03 +0300

    Переделал вид карты

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -1,4 +1,4 @@
-	<section id="img_zog" class="container_fluid img_zog wow fadeIn">
+	<section id="img_zog" class="container-fluid img_zog wow fadeIn">
 		<div class="content_img pl-5">
 			<div>
 				<h2>Развивайся вместе с</h2>
@@ -15,12 +15,24 @@
 		</div>
 	</section>
 
-	<div style="width: 640px; height: 480px" id="map"></div>
-	<input type="button" onclick="addMarkersToMap(map, 60.018701, 30.251041)" value="Считать кроликов!">
+	<!-- Map -->
+	<section class="container my_section_map wow fadeIn">
+		<div class="card card-cascade wider reverse">
+			<div class="view view-cascade overlay">
+				<div class="mt-3 my_map" id="map"></div>
+			</div>
+			<div class="card-body card-body-cascade text-center">
+				<h4 class="card-title"><strong>Наши центры</strong></h4>
+				<p class="card-text">На данной карте Вы можите наглядно увидеть расположение и рейтинг подростково -наших молодёжных центров</p>
+			</div>
+		</div>
+	</section>
+<!-- Map -->
 
-	<section id="about_us" class="container_fluid wow fadeIn my-4">
+
+	<section id="about_us" class="container-fluid wow fadeIn my-4">
 	  <div class="text-center">
-	    <h4 class="my-4 h4">О нас</h4>
+	    <h4 class="my-4">О нас</h4>
 	    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus suscipit modi sapiente illo soluta odit voluptates, quibusdam officia. Neque quibusdam quas a quis porro? Molestias illo neque eum in laborum.</p>
 	  </div>
 	</section>
