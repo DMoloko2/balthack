9f5a79e51e1b3a57cb7ab68018ab73e2f4be391a
leonya1122 <51092037+leonya1122@users.noreply.github.com>
2019-11-09 20:22:51 +0300

    редактировал запрос людей

--- application/models/People_model.php
+++ application/models/People_model.php
@@ -6,7 +6,7 @@
 		public function get_people($id)
   {
     $this->load->database();
-     $result=$this->db->query("SELECT people.id,people.name,people.sername,people.otch,whowhere.rating FROM people,section,club,whowhere WHERE whowhere.id_people=people.id AND whowhere.id_section='$id' ORDER BY whowhere.rating DESC")->result();
+     $result=$this->db->query("SELECT people.id,people.name,people.sername,people.otch,whowhere.rating FROM people,whowhere WHERE whowhere.id_people=people.id AND whowhere.id_section='$id' ORDER BY whowhere.rating DESC")->result();
      return $result;
   }
 }
