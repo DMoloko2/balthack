683cdb24041902215b2ad6b2a337bd91bb236cf6
IgorSuslov254 <igirsuslov@gmail.com>
2019-11-09 16:37:42 +0300

    Настроил Codeignaiter, По умолчанию контроллер Main_controller-Тебе ден. БД не подключал. Обновляйте. Удалил все лишние данные

---
+++ application/views/main_view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+	<head>
+		<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  	<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+		<link rel="stylesheet" 		href="<?= base_url(); ?>MDB/css/style.css">
+		<link rel="stylesheet" 		href="<?= base_url(); ?>MDB/css/my.css">
+		<link rel="stylesheet" 		href="<?= base_url(); ?>MDB/css/bootstrap.min.css">
+		<link rel="stylesheet" 		href="https://fonts.googleapis.com/css?family=Montserrat&display=swap">
+		<link rel="stylesheet" 		href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+		<title>ProgHab</title>
+	</head>
+	<body>
+    <div class="text-center mt-5">
+      проверка
+    </div>
+  </body>
+<script src="<?= base_url(); ?>MDB/js/jquery-3.4.1.min.js"></script>
+<script src="<?= base_url(); ?>MDB/js/mdb.min.js"></script>
+<script src="<?= base_url(); ?>MDB/js/bootstrap.min.js"></script>
+<script src="<?= base_url(); ?>MDB/js/script.js"></script>
+</html>
