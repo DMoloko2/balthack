cc6973cf7fcfc0c9324d012283a0444f7d9d87a2
IgorSuslov254 <igirsuslov@gmail.com>
2019-11-10 03:26:35 +0300

    обновил фото рейтига

--- application/views/rating_section_view.php
+++ application/views/rating_section_view.php
@@ -7,7 +7,7 @@
           <div class="container list-group-flush">
             <div class="list-group-item">
               <p class="chool">Центр : <?= $section->club_name; ?></p>
-              <p class="mb-0"><img src="http://www.remontbp.com/wp-content/uploads/2016/02/family-house_111215_01.jpg" class="mr-3 img_bulding_rating">Секция : <?= $section->section_name; ?><br></p>
+              <p class="mb-0"><img src="https://avatars.mds.yandex.net/get-pdb/916253/4bb994cd-e211-46f1-8860-d66098d0aadc/s1200?webp=false" class="mr-3 img_bulding_rating">Секция : <?= $section->section_name; ?><br></p>
               <p class="img_text_rating">адрес : <?= $section->address; ?></p>
               <p class="my_float">Рейтинг : <?= $section->rating; ?></p>
             </div>
