203191884e9a4155c9c5c41ff36badf597732d2d
leonya1122 <51092037+leonya1122@users.noreply.github.com>
2019-11-10 08:20:45 +0300

    запрос для статистики

--- application/models/Statistic_model.php
+++ application/models/Statistic_model.php
@@ -7,7 +7,7 @@
 		public function get_sections($id_club,$date_begin,$date_end)
       {
         $this->load->database();
-         $result=$this->db->query("SELECT COUNT(*) FROM visit,club WHERE club.id='$id_club' AND visit.data BETWEEN '".$date_begin." 00:00:00' AND '".$date_end" 23:59:00'  ")->result();
+         $result=$this->db->query("SELECT club.id,club.name,COUNT(visit.id) FROM visit,club,section WHERE visit.id_section=section.id AND section.id_club=club.id AND visit.data BETWEEN '".$data_begin." 00:00:00' AND '".$data_end." 00:00:00' GROUP BY club.id  ")->result();
          return $result;
       }
 
