5df686e92fa14d431119f3f403407a662da5252d
IgorSuslov254 <igirsuslov@gmail.com>
2019-11-10 04:52:48 +0300

    ретинг звёзды

--- application/views/rating_view.php
+++ application/views/rating_view.php
@@ -1,18 +1,34 @@
 <section class="rating container mb-5">
   <h3 class="text-center pb-2">Подростково - молодёжные центры</h3>
-  <?php foreach ($club as $club): ?>
-    <a href="#<?= $club->id; ?>">
+  <?php foreach ($club as $key): ?>
+    <a href="#<?= $key->id; ?>">
       <div class="card wow fadeInUp">
         <div class="card-body pb-0 my_-_mb25">
           <div class="container list-group-flush">
             <div class="list-group-item">
-              <p class="mb-0"><img src="http://www.remontbp.com/wp-content/uploads/2016/02/family-house_111215_01.jpg" class="mr-3 img_bulding_rating"><?= $club->name; ?></p>
-              <p class="img_text_rating">адрес : <?= $club->address; ?></p>
-              <p class="my_float">Рейтинг : <?= $club->rating; ?></p>
+              <p class="mb-0"><img src="http://www.remontbp.com/wp-content/uploads/2016/02/family-house_111215_01.jpg" class="mr-3 img_bulding_rating"><?= $key->name; ?></p>
+              <p class="img_text_rating">адрес : <?= $key->address; ?></p>
+              <p class="my_float">Рейтинг : <div id= 'rateyo-readonly-widg<?= $key->id; ?>'></div></p>
             </div>
           </div>
         </div>
       </div>
     </a>
   <?php endforeach; ?>
+  <?php print_r($club); ?>
 </section>
+
+<script>
+
+  <?php foreach ($club as $key){ ?>
+
+    $("#rateyo-readonly-widg<?php echo $key->id; ?>").rateYo({
+      rating: <?php echo $key->rating; ?>,
+      numStars: 5,
+      precision: 2,
+      minValue: 1,
+      maxValue: 5,
+      readOnly: true
+    });
+  <?php } ?>
+</script>
