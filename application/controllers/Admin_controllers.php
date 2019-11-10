cd6e494c005423aa9b509a0bf46e2d4877e1eb7d
ArchiOrange <dinka4.435@gmail.com>
2019-11-10 08:44:35 +0300

    add function getting count and name club

--- application/controllers/Admin_controllers.php
+++ application/controllers/Admin_controllers.php
@@ -8,6 +8,21 @@ class Admin_controllers extends CI_Controller {
       //показать админ панель
     }
 	}
+  public function countvisitclub($data_begin='2018-03-08',$data_end = '2019-08-20')
+  {
+    $this->load->model('Statistic_model');
+    $result = $this->Statistic_model->get_sections($data_begin,$data_end);
+    print_r($result);
+    $i = 0;
+    foreach ($result as  $value) {
+      $data['name'][$i] = $value->name;
+      $data['count'][$i] = $value->kol;
+      $i++;
+    }
+    $data['name']= json_encode($data['name']);
+    print_r($data);
+
+  }
   private function isadmin()
   {
     if ($_SESSION['admin_flag'] == 0) {
--- application/models/Statistic_model.php
+++ application/models/Statistic_model.php
@@ -4,14 +4,15 @@
 	class Statistic_model extends CI_Model
   {
 
-		public function get_sections($id_club,$date_begin,$date_end)
+		public function get_sections($data_begin,$data_end)
       {
+
         $this->load->database();
-         $result=$this->db->query("SELECT club.id,club.name,COUNT(visit.id) FROM visit,club,section WHERE visit.id_section=section.id AND section.id_club=club.id AND visit.data BETWEEN '".$data_begin." 00:00:00' AND '".$data_end." 00:00:00' GROUP BY club.id  ")->result();
+         $result=$this->db->query("SELECT club.id,club.name,COUNT(visit.id) AS kol FROM visit,club,section WHERE visit.id_section=section.id AND section.id_club=club.id AND visit.data BETWEEN '".$data_begin." 00:00:00' AND '".$data_end." 00:00:00' GROUP BY club.id  ")->result();
          return $result;
       }
 
-    }
+
   }
 
 ?>
