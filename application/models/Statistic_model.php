99a0f17ff1cb9abdebb41fd648f62a45518988e0
leonya1122 <51092037+leonya1122@users.noreply.github.com>
2019-11-10 08:03:10 +0300

    x

--- application/models/Statistic_model.php
+++ application/models/Statistic_model.php
@@ -1,13 +1,13 @@
 <?php
 	defined('BASEPATH') OR exit('No direct script access allowed');
 
-	class Section_model extends CI_Model
+	class Statistic_model extends CI_Model
   {
 
 		public function get_sections($id_club,$date_begin,$date_end)
       {
         $this->load->database();
-         $result=$this->db->query("SELECT people.id,people.name,people.sername,people.day,people.mouth,people.year,people.id_vk,people.information FROM people WHERE people.id='$id_user'")->result();
+         $result=$this->db->query("SELECT COUNT(*) FROM visit,club WHERE club.id='$id_club' AND visit.data BETWEEN '".$date_begin." 00:00:00' AND '".$date_end" 23:59:00'  ")->result();
          return $result;
       }
 
